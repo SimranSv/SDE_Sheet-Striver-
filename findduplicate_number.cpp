9991eda727e6bb7be9f9b1e1f10ef735f69b62b7
Simran Vedpathak <80968391+SimranSv@users.noreply.github.com>
2022-10-04 10:10:08 +0530

    Add files via upload

---
+++ findduplicate_number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    #define ll long long
+#define pb push_back
+#define vll vector<ll>
+#define stll set<ll>
+#define msgll multiset<ll, greater<>>
+#define msll multiset<ll>
+#define pll pair<ll, ll>
+#define mpll map<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define ss second
+#define ff first
+#define read(n, x)             \
+    for (ll i = 0; i < n; i++) \
+    {                          \
+        ll k;                  \
+        cin >> k;              \
+        x.pb(k);               \
+    }
+#define rep(i, j, k) for (ll i = j; i < k; i++)
+#define st(v) sort(v.begin(), v.end())
+#define sz(x) (ll) x.size()
+#define no cout << "No\n";
+#define yes cout << "Yes\n";
+public:
+    int findDuplicate(vector<int>& nums) {
+        ll fast=nums[0],slow=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+ return slow;
+        
+    }
+};
